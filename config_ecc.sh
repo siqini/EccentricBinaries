f468497cc8c4b6490ae3f42b64d51319b4df3c7d
waldeinseinkeit <40184634+siqini@users.noreply.github.com>
2019-07-04 00:40:37 +0200

    fixed naming mistake

--- config_ecc.sh
+++ config_ecc.sh
@@ -1,16 +1,16 @@
 Universe = vanilla
-Executable = main.sh
+Executable = main_ecc.sh
 accounting_group = aei.prod.cbc.bns
 
-Error = /work/siqi.ni/logs/necc2.err
-Output = /work/siqi.ni/logs/necc2.out
-Log = /work/siqi.ni/logs/necc2.log
+Error = /work/siqi.ni/logs/ecc2.err
+Output = /work/siqi.ni/logs/ecc2.out
+Log = /work/siqi.ni/logs/ecc2.log
 
 should_transfer_files = YES
-transfer_input_files = main.py, env.tar.gz, stand.hdf, H1L1-O1_C02_HARM_MEAN_PSD-1126051217-11203200.txt   
-transfer_output_files = necc_contour.png, fitting_factor.out
+transfer_input_files = main_ecc.py, env.tar.gz, ebank.hdf, H1L1-O1_C02_HARM_MEAN_PSD-1126051217-11203200.txt
+transfer_output_files = ecc_contour.png, ecc_fitting_factor.out
 WhenToTransferOutput = ON_EXIT
 
-RequestCpus = 6
+RequestCpus = 8
 RequestMemory = 5000
 Queue 1
